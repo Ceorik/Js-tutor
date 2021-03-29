ce1051e10d86bca7c320af2c3ebd8961bf8c9f5f
Andre <andryushka-frolov.0@yandex.ru>
2021-03-29 13:46:26 +0300

    Первый коммит

---
+++ project 1/index.js
@@ -0,0 +1,14 @@
+
+/*let clown = {
+    name: "Peter",
+    age: 18
+};
+*/
+
+//console.log(clown["name"]);
+let arr = ["/ce23c8d93707116e91e5a54539a61dbc.jpg", "/zagadki-zhaka-fresko-5.jpg"]
+alert(arr(1));
+
+let answer = prompt("Are yoy gay?");
+
+console.log(typeof(answer));
